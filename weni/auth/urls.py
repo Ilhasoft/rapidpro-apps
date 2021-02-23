6dfa673498911aed6ff0dec04402f007ef355e80
Joao <jcbalmeida@gmail.com>
2021-02-23 18:07:04 -0300

    testing serving staticfiles

--- weni/auth/urls.py
+++ weni/auth/urls.py
@@ -1,5 +1,7 @@
 from .views import check_user_legacy
 from django.conf.urls import include, url
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [url(r"^oidc/", include("mozilla_django_oidc.urls"))]
 urlpatterns += [url(r"^check-user-legacy/(?P<email>.*\\w+)/$", check_user_legacy, name="check-user-legacy")]
+urlpatterns += staticfiles_urlpatterns()

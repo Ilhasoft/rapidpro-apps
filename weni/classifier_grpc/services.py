423f5a3c88ee780e1302e3f232085c65f4d12c94
Joao <jcbalmeida@gmail.com>
2021-03-25 17:13:18 -0300

    Adds a classifier_type filter toClassifierService.List()

--- weni/classifier_grpc/services.py
+++ weni/classifier_grpc/services.py
@@ -7,8 +7,13 @@ from weni.classifier_grpc.serializers import ClassifierProtoSerializer
 class ClassifierService(AbstractService, generics.GenericService):
     def List(self, request, context):
         org = self.get_org_object(request.org_uuid, "uuid")
+        classifier_type = request.classifier_type
 
-        classifiers = org.classifiers.all()
+        if classifier_type:
+            classifiers = org.classifiers.filter(classifier_type=classifier_type)
+        else:
+            classifiers = org.classifiers.all()
+
         serializer = ClassifierProtoSerializer(classifiers, many=True)
 
         for message in serializer.message:
